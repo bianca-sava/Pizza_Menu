0fd5d4089548e2a30c9395ab1f208d4629b1f771
Bianca Sava <biancasava07@gmail.com>
2026-01-14 19:58:24 +0200

    update App.vue with localized titles, improved cart functionality, and added calorie information for menu items

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <div class="row h-100">
       <div class="col-9 p-4">
         <div class="text-center title-holder">
-          <span class="subtitle">CHOOSE YOUR FLAVOR</span>
-          <h1 class="title">THE BEST PIZZA MENU IN TOWN</h1>
+          <span class="subtitle">Papa's Pizzeria</span>
+          <h1 class="title">CEA MAI BUNA PIZZA DIN ORAS</h1>
           <p class="text">
             Alege pizza preferată!
           </p>
@@ -17,7 +17,7 @@
               <div class="p-3">
                 <h5 class="item-name">{{ item.name }}</h5>
                 <h6 class="item-price">{{ item.price }} Lei</h6>
-                <button type="button" class="add-cart">Add to Cart</button>
+                <button type="button" class="add-cart">Adauga in cos</button>
               </div>
             </div>
           </div>
@@ -25,12 +25,35 @@
       </div>
 
       <div class="col-3 cart p-4">
-        <h3 class="title">Your Basket</h3>
-        <div class="cart-empty">
-          <p class="text-center">Your basket is empty</p>
+        <h3 class="title">Cosul tau</h3>
+        <div class="order-items" v-if="cart.length > 0">
+          <div class="order-item mb-4" v-for="(item,index) in cart" :key="index">
+
+            <img class="order-item-image" :src="`/images/${item.image}`" :alt="item.name">
+            <div class="order-item-content">
+              <h5>
+                {{ item.name }} - {{item.price.toFixed(2) }} Lei
+              </h5>
+
+              <p>
+                <button type="button" class="add">+</button>
+                <button type="button" class="remove">-</button>
+              </p>
+
+              <div class="order-item-details">
+                <spam>
+                  <strong>Calorii:</strong> {{item.calories}} kcal
+                </spam>
+              </div>
+            </div>
+            <button type = "button" clas="remove-item">X</button>
+          </div>
+        </div>
+        <div class="cart-empty" v-else>
+          <p class="text-center">Cosul este gol</p>
         </div>
         <div class="total-section">
-          <h6 class="total-title">Total payment:</h6>
+          <h6 class="total-title">Total:</h6>
           <span class="amount">0 Lei</span>
         </div>
       </div>
@@ -42,15 +65,24 @@
 import { ref } from 'vue';
 
 const menu = ref([
-  { id: 1, image: "pizza-1.png", name: "Diavola", price: 30 },
-  { id: 2, image: "pizza-2.png", name: "Savoarea Mării", price: 50 },
-  { id: 3, image: "pizza-3.png", name: "Suprema", price: 55 },
-  { id: 4, image: "pizza-4.png", name: "Pizza con Funghi", price: 45 },
-  { id: 5, image: "pizza-5.png", name: "Quattro Formaggi", price: 25 },
-  { id: 6, image: "pizza-6.png", name: "Pomodoro", price: 85 },
-  { id: 7, image: "pizza-7.png", name: "Pizza Casei", price: 65 },
-  { id: 8, image: "pizza-8.png", name: "Pizza Vegetariana", price: 95 }
+  { id: 1, image: "pizza-1.png", name: "Diavola", price: 30, calories: 850 },
+  { id: 2, image: "pizza-2.png", name: "Pizza Pollo", price: 50, calories: 920 },
+  { id: 3, image: "pizza-3.png", name: "Suprema", price: 55, calories: 1050 },
+  { id: 4, image: "pizza-4.png", name: "Pizza con Funghi", price: 45, calories: 780 },
+  { id: 5, image: "pizza-5.png", name: "Quattro Formaggi", price: 55, calories: 1100 },
+  { id: 6, image: "pizza-6.png", name: "Pomodoro", price: 45, calories: 720 },
+  { id: 7, image: "pizza-7.png", name: "Pizza Casei", price: 45, calories: 980 },
+  { id: 8, image: "pizza-8.png", name: "Pizza Vegetariana", price: 35, calories: 650 }
 ]);
+
+const cart = ref([
+]);
+
+const addPizza = (item) => {
+  const pizza = menu.find(p => p.id === item.id);
+  console.log(pizza);
+};
+
 </script>
 
 <style>
